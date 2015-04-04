b521c04871c42092483012c42ca28595cd30d9ad
busyleaf <rhsnipes@yahoo.com>
2015-04-04 15:20:47 -0700

    Initial Commit

---
+++ NP 03 ShoeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NP 03 ShoeConverter
+//
+//  Created by Robert Snipes on 4/4/15.
+//  Copyright (c) 2015 Busyleaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
