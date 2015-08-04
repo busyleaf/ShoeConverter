6ddd41750091772caea269f9bb3b77bff454660c
busyleaf <rhsnipes@yahoo.com>
2015-08-04 15:13:37 -0700

    first commit

--- NP 03 ShoeConverter/ViewController.swift
+++ NP 03 ShoeConverter/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        mensShoeSizeLabel.hidden = false
+        
+        let conversionConstant = 30
+        
+        mensShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Size"
+        
+        mensShoeSizeTextField.resignFirstResponder()
+    }
 
+    @IBAction func wConvertButtonPressed(sender: UIButton) {
+        
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        womensShoeSizeLabel.hidden = false
+        
+        let conversionConstant = 30.5
+        
+        womensShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Size"
+        
+        womensShoeSizeTextField.resignFirstResponder()
+    }
 
 }
 
